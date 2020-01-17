0aa33c1be931b46ab023b09d6b565c46bcefbcbe
Juarez <juarezaksel@gmail.com>
2020-01-17 14:01:16 -0600

    added list.py

---
+++ list.py
@@ -0,0 +1,7 @@
+def print_table(table):
+    colWidths = [0] * len(table)
+    print(colWidths)
+tableData = [['apples', 'oranges', 'cherries', 'banana'],
+             ['Alice', 'Bob', 'Carol', 'David'],
+             ['dogs', 'cats', 'moose', 'goose']]
+print_table(tableData)
